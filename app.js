d618f293f80f8b8de3331a3a56086ad31628a5e8
Anupam Prakash <anupamprakash82@gmail.com>
2022-09-11 01:27:03 +0530

    taking input in console

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-prompt("heyya!");
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector('#txt-input');
+
+console.log(txtInput);
+
+function clickEventHandler() {
+    console.log("clicked!");
+    console.log("input",txtInput.value);
+  }
+
+btnTranslate.addEventListener("click", clickEventHandler);
+
